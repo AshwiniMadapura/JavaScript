c7f825199617663691cbb8a7aadfd2ef47abfee6
AshwiniMadapura <ashwinidivakar8@gmail.com>
2022-03-14 20:14:09 +0000

    updated image

--- objects/main.js
+++ objects/main.js
@@ -6,7 +6,7 @@ const person = {
     lastName:"Green",
     quote: "Try Try Try untill you succeed",
     skills: ["HTML","Css","Java Script"],
-    imgURL: "https://all-free-download.com/free-vector/download/girl_avatar_template_face_side_sketch_cartoon_character_6849757.html",
+    imgURL: "C:/VsPrjct/JavaScript/img/prof1.jpg",
 
     getFullname(){
         return `${this.firstName} ${this.lastName}`
@@ -16,22 +16,40 @@ const person = {
         const skillsHTML =this.skills.map(skill => `<li>${skill}</li>`);
         console.log(skillsHTML);
         return skillsHTML.join("\n");
+    },
+
+    getPersonHTML(){
+        const personHTML= `
+        <article class="student-card">
+            <img src=${this.imgURL} />
+            <h2> ${this.getFullname()}</h2>
+            <blockquote> ${this.quote}</blockquote>
+            <h3>Skills</h3>
+            <ul> ${this.getSkills()}</ul>
+        </article>
+        `
+        return personHTML;
+
     }
 }
 
-studentContainer.innerHTML =`
-<article class="student-card">
-    <img src=${person.imgURL} />
-    <h2> ${person.getFullname()}</h2>
-    <blockquote> ${person.quote}</blockquote>
-    <h3>Skills</h3>
-    <ul> ${person.getSkills()}</ul>
-</article>
-`
+for(let index=0; index<3; index++){
+studentContainer.innerHTML+=person.getPersonHTML();
+}
 
-person.age=30;
+// studentContainer.innerHTML =`
+// <article class="student-card">
+//     <img src=${person.imgURL} />
+//     <h2> ${person.getFullname()}</h2>
+//     <blockquote> ${person.quote}</blockquote>
+//     <h3>Skills</h3>
+//     <ul> ${person.getSkills()}</ul>
+// </article>
+// `;
+
+// person.age=30;
 // console.log(person.skills[1] + " is the skill of "+person.getFullname());
-console.log(person.getSkills());
+// console.log(person.getSkills());
 // //by calling object
 // console.log(person.firstName+ " "+person.lastName);
 // //by calling method
